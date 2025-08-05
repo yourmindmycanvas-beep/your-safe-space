14f541e21fbd76d6a4b0c82d3e405aaa48b40ca1
yourmindmycanvas-beep <yourmindmycanvas@gmail.com>
2025-08-05 21:28:11 +0100

    Add homepage route: app/page.tsx

---
+++ app/page.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main>
+      <h1>Welcome to Your Safe Space</h1>
+      <p>This is your homepage. You can now customize it!</p>
+    </main>
+  );
+}
